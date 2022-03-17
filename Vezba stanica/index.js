6d4b2e2b45e5f3efbcaea5607604934ebfb7dd07
Milos <milosmarkovic.index11@gmail.com>
2022-03-17 12:02:03 +0100

    Add files via upload

---
+++ Vezba stanica/index.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText = 55
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0
+
+function increment() {
+  count += 1;
+  countEl.textContent = count
+}
+
+function save() {
+    let saveCount = count + " - ";
+    saveEl.textContent += saveCount
+    countEl.textContent = 0
+    count = 0
+}
